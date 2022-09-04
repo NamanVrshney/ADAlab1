61f9d3aeea0dd985928d715caa1df123a09fb599
NamanVrshney <102380936+NamanVrshney@users.noreply.github.com>
2022-09-04 23:55:25 +0530

    Add files via upload

---
+++ ADA-LAB-main/toh.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+void hanoi(int x, char from, char to, char aux)
+{
+    if(x==1)
+    printf("Move disk from %c to %c\n",from,to);
+    else
+    {
+        hanoi(x-1,from,aux,to);
+        printf("Move disk from %c to %c\n",from,to);
+        hanoi(x-1,aux,to,from);
+    }
+}
+void main()
+{
+    int disk;
+    int moves;
+    
+    printf("Enter the number of disks you want to play with:");
+    scanf("%d",&disk);
+    moves=pow(2,disk)-1;
+    printf("The number of moves required is:%d\n",moves);
+    hanoi(disk,'A','C','B');
+}
